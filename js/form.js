457aa708310ca55506955e4d37fc43b6c27233ef
busarka <busarka@gmail.com>
2022-07-13 22:04:18 +0500

    Создала модуль form, подключила в main

---
+++ js/form.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+
+// НЕАКТИВНОЕ
+const infoFormAd = document.querySelector('.ad-form');
+const fieldsets = infoFormAd.querySelectorAll('fieldset');
+const filterForm = document.querySelector('.map__filters');
+const filterFormChildren = filterForm.children;
+const filterFormChildrenArray = Array.from(filterFormChildren);
+
+const notActiveStatus = function () {
+  infoFormAd.classList.add('ad-form--disabled');
+
+  fieldsets.forEach((fieldset) => {
+    fieldset.setAttribute('disabled', 'true');
+  });
+
+  filterForm.classList.add('ad-form--disabled');
+  filterFormChildrenArray.forEach((children) =>
+    children.setAttribute('disabled', 'true'));
+  console.log (filterFormChildrenArray);
+};
+// АКТИВНОЕ
+const activeStatus = function(){
+  infoFormAd.classList.remove('ad-form--disabled');
+
+  fieldsets.forEach((fieldset) => {
+    fieldset.removeAttribute('disabled');
+  });
+
+  filterForm.classList.remove('ad-form--disabled');
+
+  filterFormChildrenArray.forEach((children) =>
+    children.removeAttribute('disabled'));
+};
